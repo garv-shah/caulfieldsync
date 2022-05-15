67b41d1d1e5b80d09ecf1b2a1c451cd7b11d98bd
Garv Shah <gshah.6110@gmail.com>
2022-05-15 14:30:52 +1000

    okay actually just redirect

--- pages/api/token/[username]/[password].ts
+++ pages/api/token/[username]/[password].ts
@@ -7,18 +7,5 @@ export default async function handler(
 ) {
     const username: string = req.query['username'].toString();
     const password: string = req.query['password'].toString();
-
-    function getResponse(username: string, password: string): Promise<Response> {
-        return fetch(`https://us-central1-verdis-communications.cloudfunctions.net/token?username=${username}&password=${password}`, {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json',
-            },
-            body: null,
-        });
-    }
-
-    const response = await getResponse(username, password);
-    const returnJson = await response.json();
-    res.status(response.status).json(returnJson)
+    res.redirect(`https://us-central1-verdis-communications.cloudfunctions.net/token?username=${username}&password=${password}`)
 }
